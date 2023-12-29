9134f807cc644eb732e495697a6a08e800ffe9c4
lisposter <leigh@innomad.io>
2023-12-29 14:55:02 +0800

    feat: logo & style & etc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
-import { Theme, Tooltip } from '@radix-ui/themes'
+import Image from 'next/image'
+import { Theme, Tooltip, Button } from '@radix-ui/themes'
 
 import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
+import { HeartIcon } from '@heroicons/react/24/solid'
 
 import '@radix-ui/themes/styles.css';
 import './globals.css';
@@ -27,9 +29,17 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Theme>
-        <div className="flex flex-row h-screen">
+        <div className="flex flex-row h-screen relative z-0">
           <div className="w-12 h-screen bg-slate-800">
-            <div className="text-white">Logo</div>
+            <div className="py-2 text-center text-white">
+              <Image
+                className="inline-block"
+                width={32}
+                height={32}
+                src="https://innomad.io/favicon.png"
+                alt="投资工具箱 - by Innomad"
+              />
+            </div>
             <div className="mt-6">
               <Tooltip content="投资网址导航" side="right">
                 <Link href="/" className="py-1 text-slate-200 hover:bg-slate-600 hover:text-slate-50 transition-all duration-100">
@@ -50,8 +60,18 @@ export default function RootLayout({
             }
           </div> */}
 
-          <div className="flex-1 p-6 bg-slate-50">
+          <div className="flex-1 p-6 pb-2 bg-slate-50 flex flex-col justify-between overflow-scroll">
             {children}
+
+            <div className="mt-2 text-center text-sm text-slate-500">
+              Built with ❤️ by <a href="https://innomad.io" target="_blank" className="text-slate-500 hover:text-slate-600 hover:underline transition-all duration-100">Innomad一挪迈</a>
+            </div>
+          </div>
+
+          <div className="fixed top-3 right-6 z-10">
+            <a href="https://afdian.net/a/innomad" target="_blank" rel="noopener noreferrer" >
+              <Button size="3" color="crimson" variant="soft"><HeartIcon className="w-5 h-5" /> Sponsor</Button>
+            </a>
           </div>
         </div>
         </Theme>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,10 +12,11 @@ export default function List(props: any) {
     {
       links.map((link: any, idx: number) => {
         return (
-          <div key={idx} className="w-60 h-30 border mr-3 my-2 bg-white p-2 border-slate-300 rounded shadow-md transition-shadow duration-200 hover:shadow-xl">
+          <div key={idx} className="flex flex-col justify-between w-60 h-32 border mr-3 my-2 bg-white p-2 border-slate-300 rounded shadow-md transition-shadow duration-200 hover:shadow-xl">
+            <div>
             <div className="text-slate-800 flex">
               <Image
-                src={`https://www.google.com/s2/favicons?sz=64&domain_url=${link.url}`}
+                src={link.icon || `https://www.google.com/s2/favicons?domain=${link.url}&sz=64`}
                 alt={link.name}
                 width={16}
                 height={16}
@@ -24,16 +25,17 @@ export default function List(props: any) {
               <div className="ml-2">{link.name}</div>
             </div>
             <div className="text-sm text-slate-500 my-2">{link.desc}</div>
+            </div>
             <div className="flex justify-end">
               {
                 link.aff && (
                   <a href={link.aff} rel="noopener noreferrer" target='_blank' className="mr-2">
-                    <Button variant="surface" size="1" color="crimson" className="rounded-md">邀请链接</Button>
+                    <Button variant="surface" size="1" color="crimson" className="rounded-md w-20">邀请链接</Button>
                   </a>
                 )
               }
               <a href={link.url} rel="noopener noreferrer" target='_blank'>
-                <Button variant="outline" size="1" color="gray" className="rounded-md">访问</Button>
+                <Button variant="outline" size="1" color="gray" className="rounded-md w-20">访问</Button>
               </a>
             </div>
           </div>
